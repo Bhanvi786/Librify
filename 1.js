12c6769393178bbd33c76c86f748ae618e72e30e
Bhanvi Sahni <bhanvisahni@gmail.com>
2025-04-24 15:21:33 +0530

    Made changes in js

--- 1.js
+++ 1.js
@@ -1,6 +1,5 @@
 
-  // Set countdown duration (in seconds)
-  const totalSeconds = 3 * 3600 + 28 * 60 + 3; // 3 hours, 28 minutes, 3 seconds
+  const totalSeconds = 3 * 3600 + 28 * 60 + 3; 
   let remaining = totalSeconds;
 
   function updateTimer() {
@@ -16,32 +15,21 @@
       remaining--;
     } else {
       clearInterval(timerInterval);
-      // Optionally, show "EXPIRED" or hide the deals
     }
   }
-
-  // Initial display
   updateTimer();
-  // Update every second
   const timerInterval = setInterval(updateTimer, 1000);
 
   const loginBtn = document.getElementById('loginBtn');
   const loginModal = document.getElementById('loginModal');
   const closeLogin = document.getElementById('closeLogin');
 
-  // Show modal on login icon click
   loginBtn.onclick = function() {
       loginModal.style.display = "block";
   }
 
-  // Hide modal on close button click
   closeLogin.onclick = function() {
       loginModal.style.display = "none";
   }
 
-  // Hide modal when clicking outside the modal content
-  window.onclick = function(event) {
-      if (event.target == loginModal) {
-          loginModal.style.display = "none";
-      }
-  }
+  
