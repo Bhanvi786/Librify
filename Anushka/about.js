5a86384b628a78f8d9f9d8d084aab5b3acb364d7
Anushka <anushkaaggarwal186@gmail.com>
2025-05-04 09:38:01 +0000

    made changes in js file

--- Anushka/about.js
+++ Anushka/about.js
@@ -1,4 +1,19 @@
-
+const quotes = [
+    "A room without books is like a body without a soul. – Marcus Tullius Cicero",
+    "So many books, so little time. – Frank Zappa",
+    "Reading is essential for those who seek to rise above the ordinary. – Jim Rohn",
+    "Until I feared I would lose it, I never loved to read. One does not love breathing. – Harper Lee",
+    "Books are a uniquely portable magic. – Stephen King",
+    "There is no friend as loyal as a book. – Ernest Hemingway"
+  ];
+  
+  function generateQuote() {
+    const quoteElement = document.getElementById('randomQuote');
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    quoteElement.textContent = quotes[randomIndex];
+  }
+  
+  
   window.addEventListener('scroll', revealSections);
   
   function revealSections() {
