b091ceb17e9373c74b056090aa35651800279427
Anushka <anushkaaggarwal186@gmail.com>
2025-05-04 16:17:51 +0000

    added formspree link

--- Anushka/contact.js
+++ Anushka/contact.js
@@ -17,3 +17,18 @@ form.addEventListener("submit", async function (e) {
                 'Accept': 'application/json'
             }
         });
+
+        if (response.ok) {
+            status.textContent = "Thank you! Your message has been sent.";
+            status.style.color = "green";
+            form.reset();
+        } else {
+            const data = await response.json();
+            status.textContent = data.errors ? data.errors[0].message : "Something went wrong.";
+            status.style.color = "red";
+        }
+    } catch (error) {
+        status.textContent = "There was an error sending your message.";
+        status.style.color = "red";
+    }
+});
