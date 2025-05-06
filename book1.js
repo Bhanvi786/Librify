2e1a82c37dc9767ea88b1236e8cf14384c35cb25
Bhanvi Sahni <bhanvisahni@gmail.com>
2025-05-06 14:30:13 +0530

    Made modifications

--- book1.js
+++
@@ -1,9 +0,0 @@
-function addToCart() {
-    alert('Added to cart!');
-}
-function buyNow() {
-    alert('Proceeding to checkout!');
-}
-function addToWishlist() {
-    alert('Added to wishlist!');
-}
